eb471fdc712641bf02b2cf79bef2e54675d65382
amitavrchy <amitavrchy01@gmail.com>
2024-06-22 08:38:42 +0600

    Placeholder Data Removed

--- index.js
+++ index.js
@@ -101,10 +101,6 @@ async function run() {
             const result = await spotCollection.deleteOne(filter);
             res.send(result);
         })
-
-
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
 
     }
